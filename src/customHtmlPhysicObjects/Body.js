abfcac15c8303ef7c449ed45739f18e599f1b43f
jorge <91345902+jreyesgfi@users.noreply.github.com>
2021-12-25 14:57:22 +0100

    change the method to bring the vertices

--- src/customHtmlPhysicObjects/Body.js
+++ src/customHtmlPhysicObjects/Body.js
@@ -21,11 +21,10 @@ export default class Body extends React.Component {
 
         //////////////////////////////////
         // shape
-        const vertixTopLeff = props.vertixTopLeff || [100,100];
-
         this.width = props.width || 100;
         this.height = props.height || 150;
 
+        const vertixTopLeff = props.vertixTopLeff || [100,100];
         this.state = {angle: props.angle || 0};
 
         const initialCenter = this.obtainInitialCenter(vertixTopLeff);
@@ -33,6 +32,7 @@ export default class Body extends React.Component {
             x: initialCenter[0],
             y: initialCenter[1],
         };
+        this.center = ()=> [this.state.x, this.state.y];
 
         //////////////////////////////////
         // dinamic
@@ -189,16 +189,23 @@ export default class Body extends React.Component {
 
     vertices() {
 
-        // fix the positionating point
-        const center = this.center();
+        // establish the initial coordinates of one vertix (this case top left) regardles the rotation
+        let topLeftVertix = this.noRotatedVertixTopLeft();
 
-        // establish the initial coordinates of the vertices regardless the rotation
-        const initialVertices = [
-            [this.state.x, this.state.y],
-            [this.state.x + this.width, this.state.y],
-            [this.state.x, this.state.y + this.height],
-            [this.state.x + this.width, this.state.y + this.height]
-        ];
+        // now rotate it
+        topLeftVertix = rotateVector(this.center);
+
+        // obtain the other vertices by rotation
+        let rotatedVertices = [...Array(4).keys()].map((index)=>{
+            return rotateVector(this.center, topLeftVertix, this.angle + 90*index)
+        })
+
+        /*const initialVertices = [
+            [this.state.x - this.width/2, this.state.y + this.height/2],
+            [this.state.x + this.width/2, this.state.y + this.height/2],
+            [this.state.x - this.width/2, this.state.y - this.height/2],
+            [this.state.x + this.width/2, this.state.y - this.height/2]
+        ];*/
 
         // rotate them
         let rotatedVertices = initialVertices.map((endPoint)=>rotateVector(center,endPoint,this.state.angle));
@@ -212,9 +219,9 @@ export default class Body extends React.Component {
         ]
     }
 
-    center(){
+   noRotatedVertixTopLeft(){
         // Pass the center
-        return [this.state.x + this.width / 2, this.state.y + this.height / 2];
+        return [this.state.x - this.width/2, this.state.y + this.height/2];
     }
 
 
@@ -254,179 +261,3 @@ export default class Body extends React.Component {
         )
     }
 }
-// import {Howl, Howler} from 'howler';
-// import React from "react";
-// import './customObjects.css'
-// import { globalA, globalTimeSpan, powerLimit, reboundCoef } from "./physicConstants";
-
-// import s1 from './../sounds/s1.mp3'
-// export default class Body extends React.Component {
-//     // we should add the key parameter
-//     static id = 0;
-
-
-//     constructor(props) {
-//         // change de id
-//         const id = Body.id;
-//         Body.id += 1;
-//         props['key'] = id;
-//         super(props);
-
-//         this.state = {
-//             x: props.x || 100,
-//             y: props.y || 100,
-//             onMovement: false
-//         };
-
-//         // dinamic
-//         this.id = id;
-//         this.velX = 0;
-//         this.velY = 0;
-//         this.width = props.width || 100;
-//         this.height = props.height || 150;
-//         this.static = props.static || false;
-
-//         // style
-//         this.background = props.background || 'var(--primario-oscuro-color)';
-
-
-
-//         this.onClick = this.onClick.bind(this);
-//         this.soundOfCollision = this.soundOfCollision.bind(this);
-//         this.vertices = this.vertices.bind(this);
-//         this.posClicked = null;
-
-
-//         // add the speaker
-
-
-//         this.engine = props.engine;
-        
-
-//     }
-
-
-
-//     // start up the component
-//     componentDidMount() {
-
-//         // add this body to the world controlled by the engine
-//         this.engine.addBody(this);
-//         console.log(this.engine.objectsInWorld);
-//     }
-
-
-//     // handle mouse events
-//     setPositionClicked(position) {
-//         this.posClicked = position;
-//     }
-
-//     handleDrag(event) {
-//         const newPosClicked = [event.screenX,event.screenY];
-//         if (this.posClicked){
-//             // move the difference
-//             this.setState({ 
-//                 x: this.state.x + newPosClicked[0] - this.posClicked[0], 
-//                 y: this.state.y + newPosClicked[1] - this.posClicked[1]});
-//         }
-//         // set the position had clicked
-//         this.posClicked = newPosClicked;
-//     }
-    
-
-
-//     async setSpeed(collision, newSpeed) {
-
-//         try{
-//             // change the speed
-
-//             if (collision) {
-//                 // bounce or stop
-//                 this.velX *= this.velX**2 > 0.01 ? -reboundCoef: -1;
-//                 this.velY *= this.velY**2 > 0.01 ? -reboundCoef: -1;
-//             }
-//             else{
-//                 // else check if we are increasing the speed
-//                 this.velX += newSpeed.x || 0;
-//                 this.velY += newSpeed.y || 0;
-
-//             }
-            
-//             // set the position
-//             this.setState({ 
-//                 x: this.state.x + this.velX, 
-//                 y: this.state.y + this.velY });
-//         }
-//         catch(error){
-
-//         }
-//     }
-
-//     mySpeed(){
-//         return [this.velX, this.velY]
-//     }
-
-
-//     soundOfCollision(power){
-//         const { Howl, Howler } = require('howler');
-//         const audio = new Howl({
-//             src: s1,
-//             html5: true,
-//             onend: function () {
-//                 console.log('Finished!');
-//             }
-//         });
-//         audio.volume(power > powerLimit ? 1: (power/powerLimit)**0.5);
-//         audio.play();
-//     }
-
-//     vertices() {
-
-//         return [
-//             // Pass the vertices
-//             [
-//                 [this.state.x, this.state.y],
-//                 [this.state.x + this.width, this.state.y],
-//                 [this.state.x, this.state.y + this.height],
-//                 [this.state.x + this.width, this.state.y + this.height]
-//             ],
-//             // Pass the center
-//             [this.state.x + this.width / 2, this.state.y + this.height / 2]
-
-//         ]
-//     }
-
-
-//     render() {
-//         // style
-//         this.style =
-//         {
-//             background: this.background,
-//             width: String(this.width) + 'px',
-//             height: String(this.height) + 'px',
-//             position: 'absolute',
-//             top: String(this.state.y) + 'px',
-//             left: String(this.state.x) + 'px',
-//             //backgroundImage: 'url(' + imgUrl + ')'
-//         }
-
-//         document.body.onmousedown = function(event) { 
-//             this.handleDragStart(event);
-//         }
-//         return (
-//             <div
-//                 style={this.style}
-//                 onMouseDown={(event)=>{
-//                     this.setPositionClicked([event.screenX,event.screenY])
-//                 }}
-//                 onMouseUp={()=>{
-//                     this.setPositionClicked(null)
-//                 }}
-//                 onMouseOver={(event)=>{
-//                     this.handleDrag(event)
-//                 }}
-//                 className="physicalSquare">
-//             </div>
-//         )
-//     }
-// }
